7a61b865747b8c651fb9c89648674be75ae01183
kich_alex654@mail.ru <kich_alex654@mail.ru>
2023-02-15 16:53:09 +0300

    Типа лайки

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function App() {
+  const [likes, setLikes] = useState(0);
+
+  
   return (
     <div className="App">
-      
+    <h1> {likes}</h1> 
+    <button onClick={() => setLikes(likes+1)}>Добавить</button>
+    <button onClick={() => setLikes(likes-1)}>Убрать</button> 
     </div>
   );
 }
