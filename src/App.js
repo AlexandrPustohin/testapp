6f1fd2c98f4045772494abf3f9e7eff2088753ac
kich_alex654@mail.ru <kich_alex654@mail.ru>
2023-02-15 17:06:55 +0300

    связывание комепонентов через состояние

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React, { useState } from 'react';
 
 function App() {
   const [likes, setLikes] = useState(0);
-
+  const [value, setValue] = useState('Какойто текст из  поля');
   
   return (
     <div className="App">
     <h1> {likes}</h1> 
+    <h1>{value}</h1>
+    <input type="text" value = {value} onChange = {event => setValue(event.target.value)}/>
     <button onClick={() => setLikes(likes+1)}>Добавить</button>
     <button onClick={() => setLikes(likes-1)}>Убрать</button> 
     </div>
